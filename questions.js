7941699b8282bc246f197b70c3334eff65e290bd
waytosucceed <154772608+waytosucceed@users.noreply.github.com>
2025-08-11 13:11:09 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -7,7 +7,7 @@ var previousQuestionIndex = null // Track the previously displayed question
 var topicName = '' // Variable to store the topic name
 const submitSound = document.getElementById('submit-sound')
 
-const uniqueKey = 'Triangle'
+const uniqueKey = 'Diagonals & Exterior Angles of Polygons'
 
 // Helper function to save data in local storage under the unique key
 function saveToLocalStorage (key, value) {
@@ -1016,3 +1016,4 @@ function updateButtonText () {
     nextButton.innerHTML = 'Next'
   }
 }
+
